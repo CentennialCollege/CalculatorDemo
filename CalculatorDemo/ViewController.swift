5e4272ba22e83fabd29280cdaf7003cc54ca614a
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2020-09-29 19:53:20 -0400

    Initial Commit

---
+++ CalculatorDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CalculatorDemo
+//
+//  Created by Tom Tsiliopoulos on 2020-09-29.
+//  Copyright © 2020 CentennialCollege. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
