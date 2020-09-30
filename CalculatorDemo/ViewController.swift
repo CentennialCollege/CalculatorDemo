544e6b13ff69337457c96ff5b8530b9f2f070206
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2020-09-29 21:55:21 -0400

    added basic input actions for number buttons

--- CalculatorDemo/ViewController.swift
+++ CalculatorDemo/ViewController.swift
@@ -1,20 +1,92 @@
-//
-//  ViewController.swift
-//  CalculatorDemo
-//
-//  Created by Tom Tsiliopoulos on 2020-09-29.
-//  Copyright © 2020 CentennialCollege. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    var operand1: Double = 0.0
+    var operand2: Double = 0.0
+    var activeOperation: String = ""
 
-    override func viewDidLoad() {
+    @IBOutlet weak var ResultLabel: UILabel!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func OnNumberButton_Press(_ sender: UIButton)
+    {
+        switch sender.titleLabel!.text! {
+        case "C":
+            ResultLabel.text! = "0"
+        case "⌫":
+            ResultLabel.text!.popLast()
+            if((ResultLabel.text!.count < 1) || (ResultLabel.text! == "-"))
+            {
+                ResultLabel.text! = "0"
+            }
+        case ".":
+            if(!ResultLabel.text!.contains("."))
+            {
+                ResultLabel.text! += "."
+            }
+        case "+/-":
+            if(ResultLabel.text! != "0")
+            {
+                if(!ResultLabel.text!.contains("-"))
+                {
+                    ResultLabel.text!.insert("-", at: ResultLabel.text!.startIndex)
+                }
+                else
+                {
+                    ResultLabel.text!.remove(at: ResultLabel.text!.startIndex)
+                }
+            }
+        default:
+            if(ResultLabel.text == "0")
+            {
+                ResultLabel.text! = sender.titleLabel!.text!
+            }
+            else
+            {
+               ResultLabel.text! += sender.titleLabel!.text!
+            }
+        }
+        
+        if(ResultLabel.text!.contains("."))
+        {
+            print(Double(ResultLabel.text!)!)
+        }
+        else
+        {
+            print(Int(ResultLabel.text!)!)
+        }
+        
+        
+        
+    }
+    
+    @IBAction func OnOperatorButton_Press(_ sender: UIButton)
+    {
+        activeOperation = sender.titleLabel!.text!
+        
+        switch(activeOperation)
+        {
+        case "+":
+            print("add")
+        case "-":
+            print("minus")
+        case "x":
+            print("multiply")
+        case "÷":
+            print("divide")
+        case "%":
+            print("percent")
+        case "=":
+            print("equals")
+        default:
+            print("error")
+        }
+    }
 }
 
